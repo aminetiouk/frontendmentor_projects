91b9d75f3c855f5beb8691ba0294efbab938e90f
aminetiouk <tiouk.amine@gmail.com>
2024-11-25 23:22:28 +0100

    correct the name of type

--- mortgage-calculator/script.js
+++ mortgage-calculator/script.js
@@ -2,5 +2,5 @@ document.getElementById('calculate-btn').addEventListener('click', () => {
   const amount = parseFloat(document.getElementById('mortgage-amount').value);
   const term = parseFloat(document.getElementById('mortgage-term').value);
   const rate = parseFloat(document.getElementById('interest-rate').value);
-  const type = document.querySelector('input[name="type"]:checked');
+  const type = document.querySelector('input[name="mortgage-type"]:checked');
 });
