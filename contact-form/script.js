eaa856013915877380623c418812d3c540209d95
aminetiouk <tiouk.amine@gmail.com>
2024-12-23 22:45:03 +0100

    add error message to last name

--- contact-form/script.js
+++ contact-form/script.js
@@ -3,20 +3,39 @@ const form = document.getElementById('contact-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   
+  /* first name validation */
   const firstNameInput = document.getElementById('first-name');
   const firstName = firstNameInput.value.trim();
   
   if (firstName === '') {
-    document.querySelector('.error-message').style.display = 'block';
+    document.querySelector('#first-name + .error-message').style.display = 'block';
     firstNameInput.style.borderColor = 'red';
   } else {
-    document.querySelector('.error-message').style.display = 'none';
+    document.querySelector('#first-name + .error-message').style.display = 'none';
     firstNameInput.style.borderColor = '';
   }
   firstNameInput.addEventListener('input', () => {
     if (firstNameInput.value.trim() !== '') {
-      document.querySelector('.error-message').style.display = 'none';
+      document.querySelector('#first-name + .error-message').style.display = 'none';
       firstNameInput.style.borderColor = ''; 
+    }
+  });
+
+  /* last name validation */
+  const lastNameInput = document.getElementById('last-name');
+  const lastName = lastNameInput.value.trim();
+
+  if (lastName === '') {
+    document.querySelector('#last-name + .error-message').style.display = 'block';
+    lastNameInput.style.borderColor = 'red';
+  } else {
+    document.querySelector('#last-name + .error-message').style.display = 'none';
+    lastNameInput.style.borderColor = '';
+  }
+  lastNameInput.addEventListener('input', () => {
+    if (lastNameInput.value.trim() !== '') {
+      document.querySelector('#last-name + .error-message').style.display = 'none';
+      lastNameInput.style.borderColor = ''; 
     }
   });
 });
