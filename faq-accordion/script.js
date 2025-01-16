0fc44409b0ffb07e3b1951c56610c75f7d41825f
aminetiouk <tiouk.amine@gmail.com>
2025-01-16 22:09:19 +0100

    feat: make the Question clickable

--- faq-accordion/script.js
+++ faq-accordion/script.js
@@ -1,11 +1,15 @@
 document.querySelectorAll('.faq-article').forEach(article => {
   const icon = article.querySelector('.icon');
+  const faqQuestion = article.querySelector('.faq-question');
   const faqAnswer = article.querySelector('.faq-answer');
   let isMinus = false;
 
-  icon.addEventListener('click', () => {
+  const toggleFaq = () => {
     isMinus = !isMinus;
     icon.src = isMinus ? './assets/images/icon-minus.svg' : './assets/images/icon-plus.svg';
     faqAnswer.style.display = isMinus ? 'block' : 'none';
-  });
+  };
+
+  icon.addEventListener('click', toggleFaq);
+  faqQuestion.addEventListener('click', toggleFaq);
 });
