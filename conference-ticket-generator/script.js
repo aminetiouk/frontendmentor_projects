4bf6eb6115e828072ced21bcf69603a60597d822
aminetiouk <tiouk.amine@gmail.com>
2025-02-05 22:18:29 +0100

    feat: initial script

--- conference-ticket-generator/script.js
+++ conference-ticket-generator/script.js
@@ -1,1 +1,22 @@
-const ticket_name = document.querySelector('.ticket__name');
+const avatarPic = document.querySelector('.upload__input');
+const uploadIcon = document.querySelector('.upload__icon');
+const uploadMessage = document.querySelector('.upload__message');
+const uploadInfo = document.querySelector('.upload__info');
+const uploadError = document.querySelector('.upload__error');
+const uploadActions = document.querySelector('.upload__actions');
+const removeImage = document.querySelector('.remove-image');
+const changeImage = document.querySelector('.change-image');
+const generateButton = document.querySelector('.generate-button');
+
+// Input fields
+const fullName = document.getElementById('full-name');
+const email = document.getElementById('email');
+const githubUsername = document.getElementById('github-username');
+
+// Ticket fields
+const ticketFullName = document.querySelector('.ticket__name');
+const ticketEmail = document.querySelector('.ticket__email');
+const ticketAvatarName = document.querySelector('.ticket__avatar-name');
+const ticketAvatarGithubUsername = document.querySelector('.ticket__avatar-github-username');
+const ticketCodeNumber = document.querySelector('.ticket__code-number');
+const ticketAvatarImage = document.querySelector('.ticket__avatar-image');
