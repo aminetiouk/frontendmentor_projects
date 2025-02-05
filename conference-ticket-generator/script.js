df188448bc7f46a8e0cc759f2ca7d245d8782606
aminetiouk <tiouk.amine@gmail.com>
2025-02-05 22:20:28 +0100

    feat: implement logic to generate ticket

--- conference-ticket-generator/script.js
+++ conference-ticket-generator/script.js
@@ -20,3 +20,110 @@ const ticketAvatarName = document.querySelector('.ticket__avatar-name');
 const ticketAvatarGithubUsername = document.querySelector('.ticket__avatar-github-username');
 const ticketCodeNumber = document.querySelector('.ticket__code-number');
 const ticketAvatarImage = document.querySelector('.ticket__avatar-image');
+
+// Generate Number Code
+const generateCode = () => {
+  const code = Math.floor(Math.random() * 100000);
+  ticketCodeNumber.textContent = code;
+};
+
+avatarPic.addEventListener('change', e => {
+  e.preventDefault();
+  const file = avatarPic.files[0];
+  if (file.size > 500 * 1024) {
+    uploadInfo.style = 'display: none';
+    uploadError.style = 'display: flex';
+    return;
+  } else {
+    uploadInfo.style = 'display: flex';
+    uploadError.style = 'display: none';
+  }
+  const reader = new FileReader();
+
+  reader.onload = function() {
+    const result = reader.result;
+    uploadIcon.src = result;
+    uploadIcon.classList.remove('upload__icon');
+    uploadIcon.classList.add('avatar__picture');
+    uploadMessage.classList.add('hidden');
+    uploadActions.style = 'display: flex';
+    avatarPic.dataset.url = result; // Store the data URL in a data attribute
+  };
+
+  reader.readAsDataURL(file);
+});
+
+removeImage.addEventListener('click', e => {
+  e.preventDefault();
+  avatarPic.value = '';
+  uploadIcon.src = './assets/images/icon-upload.svg';
+  uploadIcon.classList.remove('avatar__picture');
+  uploadIcon.classList.add('upload__icon');
+});
+
+changeImage.addEventListener('click', e => {
+  e.preventDefault();
+  avatarPic.click();
+});
+
+generateButton.addEventListener('click', e => {
+  e.preventDefault();
+  const fullNameValue = fullName.value;
+  const emailValue = email.value;
+  const githubUsernameValue = githubUsername.value;
+  const avatarPicValue = avatarPic.value;
+
+  let isValid = true;
+
+  if (!avatarPicValue) {
+    uploadInfo.style = 'color: hsl(7, 88%, 67%)';
+    isValid = false;
+  } 
+
+  if (fullNameValue === '') {
+    document.querySelector('.full-name__error').style = 'display: flex';
+    isValid = false;
+  }
+
+  if (emailValue === '') {
+    document.querySelector('.email__error').style = 'display: flex';
+    isValid = false;
+  } 
+
+  if (githubUsernameValue === '') {
+    document.querySelector('.github-username__error').style = 'display: flex';
+    isValid = false;
+  }
+
+  // Add event listeners to hide error messages when input fields are filled
+  fullName.addEventListener('input', () => {
+    if (fullName.value !== '') {
+      document.querySelector('.full-name__error').style = 'display: none';
+    }
+  });
+
+  email.addEventListener('input', () => {
+    if (email.value !== '') {
+      document.querySelector('.email__error').style = 'display: none';
+    }
+  });
+
+  githubUsername.addEventListener('input', () => {
+    if (githubUsername.value !== '') {
+      document.querySelector('.github-username__error').style = 'display: none';
+    }
+  });
+
+  if (isValid) {
+    ticketFullName.textContent = fullNameValue;
+    ticketAvatarName.textContent = fullNameValue;
+    ticketEmail.textContent = emailValue;
+    ticketAvatarImage.src = avatarPic.dataset.url;
+    ticketAvatarGithubUsername.textContent = githubUsernameValue;
+
+    generateCode();
+    document.querySelector('.header').style = 'display: none';
+    document.querySelector('.ticket-form').style = 'display: none';
+    document.querySelector('.ticket').style = 'display: flex';
+  }
+});
